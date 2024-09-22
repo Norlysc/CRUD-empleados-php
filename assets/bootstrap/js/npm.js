178fbd3debb7d4d95b06a349b7a8e663dfef685d
Norlys Castañeda <castaneda30@hotmail.com>
2024-09-21 22:25:11 -0500

    ajustes en archivos comentarios

--- assets/bootstrap/js/npm.js
+++ assets/bootstrap/js/npm.js
@@ -11,3 +11,9 @@ require('../../js/popover.js')
 require('../../js/scrollspy.js')
 require('../../js/tab.js')
 require('../../js/affix.js')
+
+/*
+Este bloque importa varios módulos de Bootstrap para habilitar la funcionalidad de componentes interactivos en la aplicación, 
+como alertas, botones, carruseles, modales y tooltips. 
+Estas funcionalidades mejoran la experiencia del usuario y la usabilidad de la interfaz.
+*/
